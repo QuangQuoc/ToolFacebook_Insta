d7270b57f4de7412c139427e36523eed1eb99fc6
Truong Quang Quoc <quangquoc969490@gmail.com>
2020-03-04 11:02:27 +0700

    thêm lưu từng bước

---
+++ CreateAccountsFacebook/Repositories/AccountsRepository.cs
@@ -0,0 +1,34 @@
+﻿using CreateAccountsProject.Models;
+using CreateAccountsProject.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlLdPlayer.Repositories
+{
+    public class AccountsRepository
+    {
+        Context context = new Context();
+
+        public bool UpdateAccount(int id, Account acc)
+        {
+            Account accDb = context.Accounts
+                .Where(a => a.Id == id)
+                .FirstOrDefault();
+
+            if (accDb != null)
+            {
+                accDb.Update(acc);
+                context.SaveChanges();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
